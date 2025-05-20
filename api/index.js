cb5c6d456eab693f50e26448908e9928a80a9f37
kaito-77 <azxcyg1@gmail.com>
2025-05-20 10:48:26 +0800

    add api/index.js

---
+++ api/index.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+const express = require('express')
+const app = express()
+
+// get the port from env variable
+const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
